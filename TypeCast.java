c5770776c9e12f30256fc914f73db0b63a84b658
aniketk03 <aniketkshirsagar05@gmail.com>
2024-02-26 20:56:31 +0530

    TypeCasting Practice

--- TypeCast.java
+++ TypeCast.java
@@ -6,6 +6,16 @@
 public class TypeCast {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+       byte a = 30;
+        int b = (int) a;
+        short c = (short)b;
+        double d = (double)c;
+        float e = (float)d;
+        System.out.println("Byte :" + a );
+        System.out.println("Int :" + b );
+        System.out.println("Short :" + c );
+        System.out.println("Double :" + d );
+        System.out.println("Float :" + e );
+    
     }
 }
