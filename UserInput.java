9ae6b1555698e20d5fd02ade45188e4cf075d5ab
aniketk03 <aniketkshirsagar05@gmail.com>
2024-02-26 22:56:20 +0530

    User Input Files

---
+++ UserInput.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class UserInput
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter int , string, char, float, double :");
+        int a = sc.nextInt();
+        sc.nextLine();
+        String b = sc.nextLine();
+        char c = sc.next().charAt(0);
+        float d  = sc.nextFloat();
+        double e = sc.nextDouble();
+
+        System.out.println(a + b + c + d + e);
+
+        
+    }
+}
+
+
+
+
+
