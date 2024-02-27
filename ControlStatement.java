c8b32e6ae9ab9535d2128374cd6b781deae01e42
aniketk03 <aniketkshirsagar05@gmail.com>
2024-02-27 20:20:00 +0530

    ELSE IF LADDER

--- ControlStatement.java
+++ ControlStatement.java
@@ -8,6 +8,14 @@ public class ControlStatement {
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
+        
+        /* */ System.out.println("1. Sunday");
+        System.out.println("2. Monday");
+        System.out.println("3. Tuesday");
+        System.out.println("4. Wednesday");
+        System.out.println("5. Thursday");
+        System.out.println("6. Friday");
+        System.out.println("7. Saturday");
         System.out.print("Enter The Number ::");
         int n = sc.nextInt();
         System.out.println("Your Entered Number is :: " + n);
@@ -21,6 +29,46 @@ public class ControlStatement {
             System.out.println("This is Odd Number"); */
 
         //
+        //ELSE IF LADDER
+        if(n==1)
+        {
+            System.out.println("Today is Sunday");
+        }
+        else if(n==2)
+        {
+            System.out.println("Today is Monday");
+        }
+        else if(n==3)
+        {
+            System.out.println("Today is Tuesday");
+        }
+        
+       else if(n==4)
+        {
+            System.out.println("Today is Wednesday");
+        }
+        else if(n==5)
+        {
+            System.out.println("Today is Thursday");
+        }
+        else if(n==6)
+        {
+            System.out.println("Today is Friday");
+        }
+        else if(n==7)
+        {
+            System.out.println("Today is Saturday");
+        }
+
+
+
+
+
+
+
+
+
+
         sc.close();
     }
 }
