d4e18f8e1a2067c6a9c153ee28a4948931bfe2d4
aniketk03 <aniketkshirsagar05@gmail.com>
2024-02-29 22:55:16 +0530

    Switch Case

--- Switchcase.java
+++ Switchcase.java
@@ -24,7 +24,7 @@ public class Switchcase {
                 System.out.println("Your Selected Language is C++");
                 break;
         }
-        sc.close();
+       sc.close();
 
     }
     
