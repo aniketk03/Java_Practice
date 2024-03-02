befa2539469c37868b0c6090264678dab934fd93
aniketk03 <aniketkshirsagar05@gmail.com>
2024-03-02 20:23:03 +0530

    Hollow rectangle pattern

--- Loops.java
+++ Loops.java
@@ -3,13 +3,31 @@
 
 
 
-
 public class Loops {
 
     public static void main(String[] args) {
-        
+    //for loop design patterns  
+    //hollow rectangle pattern  
+    // 
+    //    *****   
+    //    *   *
+    //    *   * 
+    //    *****
+   
+        int i=1, j=1, ni=4, nj=5;
+        for(i=1; i<=ni; i++)
+        {
+            for(j=1; j<=nj; j++)
+            {
+                if(i==1 || j==1 || i ==4 || j ==5)
+                {
+                System.out.print("*");
+                }
+                else
+                System.out.print(" ");
+            }
+            System.out.println(" ");//this for new line after each line
+        }
 
-
-
-    }
+}
 }
