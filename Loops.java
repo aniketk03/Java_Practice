564c1929db4247302b168f05bfed1943bbc54a3c
aniketk03 <aniketkshirsagar05@gmail.com>
2024-03-02 20:30:41 +0530

    Solid rectangle pattern

--- Loops.java
+++ Loops.java
@@ -7,11 +7,11 @@ public class Loops {
 
     public static void main(String[] args) {
     //for loop design patterns  
-    //hollow rectangle pattern  
+    //Solid rectangle pattern  
     // 
     //    *****   
-    //    *   *
-    //    *   * 
+    //    *****   
+    //    ***** 
     //    *****
    
         int i=1, j=1, ni=4, nj=5;
@@ -19,12 +19,7 @@ public class Loops {
         {
             for(j=1; j<=nj; j++)
             {
-                if(i==1 || j==1 || i ==4 || j ==5)
-                {
-                System.out.print("*");
-                }
-                else
-                System.out.print(" ");
+               System.out.print("*");
             }
             System.out.println(" ");//this for new line after each line
         }
