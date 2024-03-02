3e0e045c5a359ef206fff6e1e5782114d089117c
aniketk03 <aniketkshirsagar05@gmail.com>
2024-03-02 19:26:28 +0530

    While Loop

---
+++ Loops.java
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+public class Loops {
+
+    public static void main(String[] args) {
+        //While Loop
+        //this is entry control loop it firstly check condition
+        int i=1, n=5;
+        while(i<=n)
+        {
+                System.out.println("this this while loop");
+                i++;
+        }
+
+    }
+}
