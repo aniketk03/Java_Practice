b7cdf847882832563c8d4aa24b3f50ce466da4d9
aniketk03 <aniketkshirsagar05@gmail.com>
2024-03-02 20:04:20 +0530

    for solid rectangle pattern

--- Loops.java
+++ Loops.java
@@ -7,14 +7,9 @@
 public class Loops {
 
     public static void main(String[] args) {
-        //do While Loop
-        //this is exit control loop it once execute then check condition
-        int i=1, n=5;
         
-        do{
-                System.out.println("this this while loop");
-                i++;
-        }while(i<=n);
+
+
 
     }
 }
