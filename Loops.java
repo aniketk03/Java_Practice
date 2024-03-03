dddabee46df239746d55649031e54491235ab02b
aniketk03 <aniketkshirsagar05@gmail.com>
2024-03-03 14:58:29 +0530

    Half Pyramid pattern

--- Loops.java
+++ Loops.java
@@ -7,17 +7,17 @@ public class Loops {
 
     public static void main(String[] args) {
     //for loop design patterns  
-    //Solid rectangle pattern  
+    //Half Pyramid pattern  
     // 
-    //    *****   
-    //    *****   
-    //    ***** 
+    //    *   
+    //    **   
+    //    *** 
     //    *****
    
-        int i=1, j=1, ni=4, nj=5;
+        int i=1, j=1, ni=4, nj=4;
         for(i=1; i<=ni; i++)
         {
-            for(j=1; j<=nj; j++)
+            for(j=1; j<=i; j++)
             {
                System.out.print("*");
             }
