498a5e6155b94998a1654e811ff47c3e30498811
aniketk03 <aniketkshirsagar05@gmail.com>
2024-03-05 23:19:38 +0530

    Floyads Traingle

--- Loops.java
+++ Loops.java
@@ -7,20 +7,22 @@ public class Loops {
 
     public static void main(String[] args) {
     //for loop design patterns  
-    //Half Pyramid pattern With Numbers  
+    //Floyad's Triangle 
     // 
     //   1  
-    //   12 
-    //   123  
-    //   1234
+    //   23 
+    //   456  
+    //   78910
     
    
         int i=4, j=1, ni=4, nj=4;
+        int number =1;
         for(i=1; i<=4; i++)
         {
             for(j=1; j<=i; j++)
             {
-               System.out.print(j);
+               System.out.print(number);
+               number++;
             }
             System.out.println(" ");//this for new line after each line
         }
