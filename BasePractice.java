40225543bf9883c69eabf61b2bf90e906ee9cc21
aniketk03 <aniketkshirsagar05@gmail.com>
2024-03-06 17:57:02 +0530

    Problem 4

--- BasePractice.java
+++ BasePractice.java
@@ -1,31 +1,26 @@
 import java.util.Scanner;
 
 
-//Write a function which takes in 2 numbers and returns the greater of those two.
+//Write a function that takes in the radius as input and returns the circumference of a circle.
 
 
 
 public class BasePractice {
 
-    public static int greaterNumber (int n1, int n2)
+    public static float circleCircum (float pi, float n1)
     {
-        if(n1>n2)
-        {
+       
             
-            return n1;
-        }
-        else
-            
-       return n2;
+       return 2*pi*n1;
         
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter The Two Numbers Number :");
-        int n1 = sc.nextInt();
-        int n2 = sc.nextInt();
-        int GreaterNumber = greaterNumber(n1, n2);
-        System.out.println("Greater Number is :: " + GreaterNumber);
+        System.out.println("Enter The Radius :");
+        Float n1 = sc.nextFloat();
+        float pi = 3.14159f;
+        float cir = circleCircum(pi,n1);
+        System.out.println("Circumference is :: " + cir);
        sc.close();
 
     }
