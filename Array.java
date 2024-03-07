6911faf7adce56bbf7bcb0162d605aee9167ee6a
aniketk03 <aniketkshirsagar05@gmail.com>
2024-03-07 23:21:45 +0530

    Two Dimensional Array

--- Array.java
+++ Array.java
@@ -1,26 +1,36 @@
 import java.util.Scanner;
 
-//Single Dimension array
+//Two Dimension array
 
 public class Array {
      
     public static void main(String[] args) {
         
-        int i;
+        int i,j;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of array");
-        int size = sc.nextInt();
-        int [] arr = new int[size];
+        System.out.println("Enter the size of Row and Column");
+        int row = sc.nextInt();
+        int column = sc.nextInt();
+        int [][] arr = new int[row][column];
         System.out.println("Enter The Elements :: ");
-        for(i=0; i<size; i++)
+        for(i=0; i<row; i++)
         {
-             arr[i] = sc.nextInt();
+            for(j=0; j<column; j++)
+            {
+             arr[i][j] = sc.nextInt();
+            }
         }
 
-        for(i=0; i<size; i++)
+        for(i=0; i<row; i++)
         {
-            System.out.println("Array is :: " +arr[i]);
+            for(j=0; j<column; j++)
+            {
+             System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
         }
+        sc.close();
     }
+    
     
 }
