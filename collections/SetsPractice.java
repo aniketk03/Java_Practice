fe93394c56226afe4f1ebfe90e345f531fc8c34e
aniketkshirsagar01 <1002kshirsagaras@gmail.com>
2024-06-22 15:43:49 +0530

    Collections

--- collections/SetsPractice.java
+++ collections/SetsPractice.java
@@ -1,2 +1,24 @@
-package collections;public class SetsPractice {
+package collections;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetsPractice {
+    public static void main(String[] args) {
+        Set<Integer> values = new TreeSet<>();
+        values.add(1);
+        values.add(1);
+        values.add(4);
+        values.add(2);
+        System.out.println(values);
+
+        values.remove(2);
+        System.out.println(values);
+
+        System.out.println(values.size());
+        System.out.println(values.contains(3));
+        values.clear();
+        System.out.println(values);
+    }
 }
